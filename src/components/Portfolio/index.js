7eaaec64f138a1bdd4ee0a03ce1bd3378256cda7
deepakrajmohanraj <115361961+deepakrajmohanraj@users.noreply.github.com>
2024-06-18 23:57:20 -0400

    Add projects to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
+import Project1 from '../../assets/images/Melanoma.png'; 
+import Project2 from '../../assets/images/Car.png'; 
+import Project3 from '../../assets/images/Credit default.png'; 
+import Project4 from '../../assets/images/Buffalo.png'; 
+import Project5 from '../../assets/images/Time series.png'; 
 import "./index.scss";
 
-const Portfolio = () => { 
+const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
 
     useEffect(() => {
@@ -14,37 +19,66 @@ const Portfolio = () => {
         return () => {
             clearTimeout(timer);
         }
-    });
+    }, []);
 
-   
+    const portfolio = [
+        {
+            name: "Melanoma Skin Cancer Detection using CNN",
+            // description: "Short description of Project One",
+            image: Project1,
+            url: "https://github.com/deepakrajmohanraj/melanoma-skin-cancer-detection"
+        },
+        {
+            name: "Classification of Car Damage",
+            // description: "Short description of Project Two",
+            image: Project2,
+            url: "https://github.com/deepakrajmohanraj/Classification-of-Car-Damage"
+        },
+        {
+            name: "Credit Defaulter Analysis and Prediction",
+            // description: "Short description of Project Three",
+            image: Project3,
+            url: "https://github.com/deepakrajmohanraj/Credit-Defaulter-Analysis-and-Prediction"
+        },
+        {
+            name: "PostgreSQL Database for Properties in Buffalo",
+            // description: "Short description of Project Four",
+            image: Project4,
+            url: "https://github.com/deepakrajmohanraj/PostgreSQL-Database-for-Properties-in-Buffalo"
+        },
+        {
+            name: "Time Series Forecasting Methods on Retail Store Sales Data",
+            // description: "Short description of Project Five",
+            image: Project5,
+            url: "https://github.com/deepakrajmohanraj/Time-Series-Forecasting-Methods-on-Retail-Store-Sales-Data"
+        }
+    ];
 
     const renderPortfolio = (portfolio) => {
         return (
             <div className="images-container">
                 {
-                    portfolio.map((port, idx) => {
-                        return (
-                            <div className="image-box" key={idx}>
-                                <img 
+                    portfolio.map((port, idx) => (
+                        <div className="image-box" key={idx}>
+                            <img
                                 src={port.image}
                                 className="portfolio-image"
-                                alt="portfolio" />
-                                <div className="content">
-                                    <p className="title">{port.name}</p>
-                                    <h4 className="description">{port.description}</h4>
-                                    <button
-                                        className="btn"
-                                        onClick={() => window.open(port.url)}
-                                    >View</button>
-                                </div>
+                                alt="portfolio"
+                            />
+                            <div className="content">
+                                <p className="title">
+                                    <a href={port.url} target="_blank" rel="noopener noreferrer">
+                                        {port.name}
+                                    </a>
+                                </p>
+                                {/* <h4 className="description">{port.description}</h4> */}
                             </div>
-                        )
-                    })
+                        </div>
+                    ))
                 }
             </div>
         );
     }
-
 
     return (
         <>
@@ -56,7 +90,7 @@ const Portfolio = () => {
                         idx={15}
                     />
                 </h1>
-                
+                {renderPortfolio(portfolio)}
             </div>
             <Loader type="pacman" />
         </>
